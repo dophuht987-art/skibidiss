9480f217aa4262c749982215fbe1508875fefdb0
dophuht987-art <dophuht987@gmail.com>
2026-03-17 19:22:03 +0700

    Create ghostz.lua

---
+++ ghostz.lua
@@ -0,0 +1,7 @@
+-- this script created at https://lambloxfruitscript.vercel.app/
+getgenv().ScriptTitle = "GHOSTZ"
+getgenv().ScriptSubTitle = "ghostz hub"
+getgenv().ScriptImage = "https://i.ibb.co/zV3hpDDJ/z7621137916644-e96796d3902b12f9bb35ab0737a7707d.jpg"
+getgenv().ScriptAuthorName = "ghotsz56"
+getgenv().ScriptAuthorSubTitle = "tiktok@ ghotsz56"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
